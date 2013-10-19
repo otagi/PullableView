bab373cdc5e25dd752e1edc430e31c1dc80963d8
Tiago Mendes-Costa <tiago@mendes-costa.net>
2013-10-19 17:01:54 +0200

    Swapped more ViewController's variables & the filename to snake_case. Removed the on_change_state callback for the pull_down_view.

--- app/view_controller.rb
+++ app/view_controller.rb
@@ -9,11 +9,11 @@ class ViewController < UIViewController
     
     @pullable = true
     
-    xOffset = (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) ? 224 : 0
+    x_offset = (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) ? 224 : 0
     
-    @pull_up_view = StyledPullableView.alloc.initWithFrame(CGRectMake(xOffset, 0, 320, PUlL_UP_VIEW_HEIGHT)).tap do |view|
-      view.opened_center     = CGPointMake(160 + xOffset, (self.view.frame.size.height - PUlL_UP_VIEW_HEIGHT / 2))
-      view.closed_center     = CGPointMake(160 + xOffset, self.view.frame.size.height + PUlL_UP_VIEW_HEIGHT / 2 - 30)
+    @pull_up_view = StyledPullableView.alloc.initWithFrame(CGRectMake(x_offset, 0, 320, PUlL_UP_VIEW_HEIGHT)).tap do |view|
+      view.opened_center     = CGPointMake(160 + x_offset, (self.view.frame.size.height - PUlL_UP_VIEW_HEIGHT / 2))
+      view.closed_center     = CGPointMake(160 + x_offset, self.view.frame.size.height + PUlL_UP_VIEW_HEIGHT / 2 - 30)
       view.center            = view.closed_center
       view.handle_view.frame = CGRectMake(0, 0, 320, 40)
       view.should_receive_touch { @pullable }
@@ -41,14 +41,11 @@ class ViewController < UIViewController
       @pull_up_view.addSubview(label)
     end
     
-    @pull_down_view = StyledPullableView.alloc.initWithFrame(CGRectMake(xOffset, 0, 320, PULL_DOWN_VIEW_HEIGHT)).tap do |view|
-      view.opened_center = CGPointMake(160 + xOffset, PULL_DOWN_VIEW_HEIGHT / 2)
-      view.closed_center = CGPointMake(160 + xOffset, - PULL_DOWN_VIEW_HEIGHT / 2 + 30)
+    @pull_down_view = StyledPullableView.alloc.initWithFrame(CGRectMake(x_offset, 0, 320, PULL_DOWN_VIEW_HEIGHT)).tap do |view|
+      view.opened_center = CGPointMake(160 + x_offset, PULL_DOWN_VIEW_HEIGHT / 2)
+      view.closed_center = CGPointMake(160 + x_offset, - PULL_DOWN_VIEW_HEIGHT / 2 + 30)
       view.center        = view.closed_center
       view.should_receive_touch { @pullable }
-      view.on_change_state do |opened|
-        @pull_up_label.text = opened ? "Now I'm open!" : "Now I'm closed, pull me up again!"
-      end
       self.view.addSubview(view)
     end
     
@@ -63,7 +60,7 @@ class ViewController < UIViewController
     end
   end
 
-  def shouldAutorotateToInterfaceOrientation(interfaceOrientation)
-    UIInterfaceOrientationIsPortrait(interfaceOrientation)
+  def shouldAutorotateToInterfaceOrientation(orientation)
+    UIInterfaceOrientationIsPortrait(orientation)
   end
 end
